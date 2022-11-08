cac28b3fff1674219fee0e0915e5d180f3d22d9d
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-11-09 04:55:46 +0600

    db added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middle wares
@@ -15,18 +15,47 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kvqywrf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('sportsPhotography').collection('services');
+        const reviewCollection = client.db('sportsPhotography').collection('reviews');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+
+            // review api
+            app.post('/reviews', async (req, res) => {
+                const review = req.body;
+                const result = await reviewCollection.insertOne(review)
+                res.send(result);
+            })
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('sports photography running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`sports photography running on ${port}`);
 })
