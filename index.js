bd6cd346f3158b65aaab0a9392eb48b3513707f0
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-11-10 21:33:48 +0600

    final check

--- index.js
+++ index.js
@@ -90,12 +90,12 @@ async function run() {
                 res.send(review)
             });
 
-            // app.delete('reviews/:id', async (req, res) => {
-            //     const id = req.params.id;
-            //     const query = { _id: ObjectId(id) };
-            //     const result = await reviewCollection.deleteOne(query);
-            //     res.send(result);
-            // })
+            app.delete('reviews/:id', async (req, res) => {
+                const id = req.params.id;
+                const query = { _id: ObjectId(id) };
+                const result = await reviewCollection.deleteOne(query);
+                res.send(result);
+            })
         })
 
     }
